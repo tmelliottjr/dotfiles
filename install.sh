5e40e47d859ba236b44fdaaa0463ef8a8ae37b64
Tom Elliott <tmelliottjr@github.com>
2023-04-11 09:33:09 -0400

    initial

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+zshrc() {
+    echo "==========================================================="
+    echo "             cloning powerlevel10k                         "
+    echo "-----------------------------------------------------------"                    
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    echo "==========================================================="
+    echo "                 cloning zsh-zsh-nvm                       "
+    echo "-----------------------------------------------------------"                 
+    git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-nvm
+    echo "==========================================================="
+    echo "                  Import zshrc                             "
+    echo "-----------------------------------------------------------"
+    cat .zshrc > $HOME/.zshrc
+}
+
+zshrc
