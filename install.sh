66b01c7be6c49957ae1cb47d723bed6fbc21dd2e
Tom Elliott <tmelliottjr@github.com>
2023-04-11 09:35:31 -0400

    exa

--- install.sh
+++ install.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
 
 zshrc() {
-    echo "==========================================================="
-    echo "             cloning powerlevel10k                         "
-    echo "-----------------------------------------------------------"                    
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-    echo "==========================================================="
-    echo "                 cloning zsh-zsh-nvm                       "
-    echo "-----------------------------------------------------------"                 
-    git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-nvm
-    echo "==========================================================="
-    echo "                  Import zshrc                             "
-    echo "-----------------------------------------------------------"
-    cat .zshrc > $HOME/.zshrc
+  echo "==========================================================="
+  echo "             cloning powerlevel10k                         "
+  echo "-----------------------------------------------------------"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+  echo "==========================================================="
+  echo "                 cloning zsh-zsh-nvm                       "
+  echo "-----------------------------------------------------------"
+  git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-nvm
+  echo "==========================================================="
+  echo "                  Import zshrc                             "
+  echo "-----------------------------------------------------------"
+  cat .zshrc >$HOME/.zshrc
 }
 
+packages() {
+  echo "==========================================================="
+  echo "                  Install Exa                              "
+  echo "-----------------------------------------------------------"
+  apt install exa
+}
+
+packages
 zshrc
