5b725c4f6524e2e5b4d7ecf4199e7f0f57fc43c3
Tom Elliott <tmelliottjr@gmail.com>
2025-05-17 10:47:46 -0400

    testing

--- install.sh
+++ install.sh
@@ -2,30 +2,75 @@
 
 zshrc() {
   echo "==========================================================="
-  echo "             cloning powerlevel10k                         "
-  echo "-----------------------------------------------------------"
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-  echo "==========================================================="
   echo "                 cloning zsh-zsh-nvm                       "
   echo "-----------------------------------------------------------"
   git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-nvm
   echo "==========================================================="
   echo "                  Import zshrc                             "
   echo "-----------------------------------------------------------"
-  cat .zshrc > $HOME/.zshrc
+  cat .zshrc >$HOME/.zshrc
   echo "==========================================================="
-  echo "             import powerlevel10k                          "
+  echo "              Setup Starship config                        "
   echo "-----------------------------------------------------------"
-  cat .p10k.zsh > $HOME/.p10k.zsh
+  mkdir -p $HOME/.config
+  cat starship.toml >$HOME/.config/starship.toml
+}
+
+install_starship() {
+  echo "==========================================================="
+  echo "              Installing Starship                          "
+  echo "-----------------------------------------------------------"
+  # Use the official installer script with -y flag for non-interactive install
+  curl -sS https://starship.rs/install.sh | sh -s -- -y
+}
+
+install_fabulous() {
+  echo "==========================================================="
+  echo "               Installing fabulous                         "
+  echo "-----------------------------------------------------------"
+
+  # Install fabulous using pip with --yes flag for non-interactive install
+  if
+    command -v pip3 &
+    >/dev/null
+  then
+    pip3 install --yes fabulous
+  elif
+    command -v pip &
+    >/dev/null
+  then
+    pip install --yes fabulous
+  else
+    echo "Could not install fabulous: pip not found"
+    echo "Please install pip and try again"
+  fi
 }
 
 packages() {
   echo "==========================================================="
-  echo "                  Install Exa                              "
+  echo "               Installing packages                         "
   echo "-----------------------------------------------------------"
-  sudo apt install exa
+
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    brew install exa
+  elif [ -x "$(command -v apt)" ]; then
+    # Debian/Ubuntu
+    sudo apt-get install -y exa
+  elif [ -x "$(command -v dnf)" ]; then
+    # Fedora
+    sudo dnf install -y exa
+  elif [ -x "$(command -v pacman)" ]; then
+    # Arch
+    sudo pacman -S --noconfirm exa
+  else
+    echo "Could not install packages: Unsupported package manager"
+  fi
 }
 
+# Install components
+install_starship
+install_fabulous
 zshrc
 packages
 
